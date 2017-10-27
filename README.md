6c11f434ed53b49672fcad96a14d74cc19c308f4
sudipta <sdinda660>
2017-10-27 18:21:43 +0530

    hamburger menu with nav

---
+++ README.md
@@ -0,0 +1,1 @@
+# hamburger-menu-with-responsive-navigation
---
+++ script.js
@@ -0,0 +1,34 @@
+// // open mobile menu
+$('.hamburger-menu-wrapper').click(function (e) {
+    e.preventDefault();
+    $('nav').slideToggle();
+    $(this).toggleClass('open');
+});
+
+(function () {
+    'use strict';
+    $('.hamburger-menu').click(function (e) {
+        e.preventDefault();
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
+        } else {
+            $(this).addClass('active');
+            $('.hamburger-menu-wrapper').toggleClass('bounce-effect');
+        }
+    })
+})();
+
+
+$(".parent").click(function () {
+    $(this).children(".sub-nav").slideToggle();
+    $(this).children(".list-menu").slideToggle();
+});
+
+$("ul").click(function (p) {
+    p.stopPropagation();
+});
+
+
+//////////
+
